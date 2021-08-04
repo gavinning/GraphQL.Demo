75242725fe52cc4b36b89a5c7b78b1743d85fc38
gavinning <gavinning@qq.com>
2021-08-04 22:31:53 +0800

    init

---
+++ GraphQL.Demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace GraphQL.Demo {
+    class MainClass {
+        public async static Task Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            await demoGraphql();
+        }
+
+        async static Task demoGraphql() {
+            //var graphQLClient = new GraphQLHttpClient("https://api.github.com/graphql");
+            var graphQLClient = new GraphQLHttpClient("http://localhost:9902/graphql", new NewtonsoftJsonSerializer());
+
+            var usersRequest = new GraphQLRequest {
+                Query = @"
+                    {
+                        users {
+                            username
+                        }
+                    }"
+            };
+            Console.WriteLine("12333");
+
+            var resoponse = await graphQLClient.SendQueryAsync<ResponseType>(usersRequest);
+
+            Console.WriteLine(resoponse.Data.Users[0].username);
+
+            //return Task<'ok'>
+        }
+    }
+}
+
+public class ResponseType {
+    public User[] Users { get; set; }
+}
+
+public class User {
+    public string username { get; set; }
+}
